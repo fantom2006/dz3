cab34c4f0098b583c19b97d6b35cd261210e5dfb
fantom2006 <alibaeveldiar01@gmail.com>
2023-07-26 16:29:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// const phoneNumber = "+996500101010"
+// const message = 'hello'
+const sendSMS = (phoneNumber, message) => {
+    console.log(`Отправлено SMS на номер ${phoneNumber}: "${message}"`);
+  };
+  
+const phoneNumber = "+996500101010"
+const message = 'hello'
+
+const send = (`Отправлено SMS на номер ${phoneNumber}: ${message}`)
+console.log(send);
+
+  
